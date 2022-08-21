bea847c90be82b28e6b0ddc8b349ca5c7f93326e
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-08-21 00:46:13 -0400

    adding some java

--- Assets/script.js
+++ Assets/script.js
@@ -5,35 +5,41 @@ var weatherCall = "";
 var cityEl = document.querySelector("#city")
 var searchE1 = document.querySelector("#search-btn")
 var cityDate = document.querySelector("#cityDate")
+var nameE1 = document.querySelector(city-name)
+var currentTempE1 = document.querySelector("tempertaure")
+var currentHumidityE1 = document.querySelector("humidity")
+var currentWindE1 = document.querySelector("windspeed")
+var 
+Fetch('https://api.openweathermap.org/data/2.5/weather?q="+ cityName + "&appid-8a42d43f7d7dc180da5b1e51890e67dc')
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    var uvindex =("https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=8a42d43f7d7dc180da5b1e51890e67dc" ),
+    return fetch(uvindex)
+  })
+    .then(function (res) {
+      return res.json();
+    })
+  .then(function (data) {
+    console.log(data);
+  }); 
+};
 
-// Fetch('https://api.openweathermap.org/data/2.5/weather?q="+ cityName + "=8a42d43f7d7dc180da5b1e51890e67dc')
-  // .then(function (res) {
-    // return res.json();
-  // })
-  // .then(function (data) {
-    // console.log(data);
-    // var uvindex =("https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=8a42d43f7d7dc180da5b1e51890e67dc" ),
-    // })
-    // .then(function (res) {
-      // return res.json();
-    // })
-  // .then(function (data) {
-    // console.log(data);
-  // }); 
-// 
-// 
-// var citySearch = Function(event) {
-  // event.preventDefault();
-// 
-// var searchdeafault();
-// 
-// var searchTerm = cityEl.ariaValueMax.trim();
-// 
-// if (searchTerm) {
-  // weatherCall(searchTerm)
-  // searchTerm = "";
-// } else console.warn("you must choose a city");
-// 
-// 
-// }
-// searchE1.addEventListener("click", citySearch)
+var citySearch = Function(event) {
+  event.preventDefault();
+  
+  var searchTerm = cityEl.vaule.trim();
+  
+var searchdeafault();
+
+
+if (searchTerm) {
+  weatherCall(searchTerm)
+  searchTerm = "";
+} else console.warn("you must choose a city");
+
+
+}
+searchE1.addEventListener("click", citySearch)
